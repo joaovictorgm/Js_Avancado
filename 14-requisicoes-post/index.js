bc1bee9c6113fc4cf6d9bc792d26a141760a7862
joaovictorgm <94790386+joaovictorgm@users.noreply.github.com>
2023-11-21 22:13:06 -0300

    aprendendo sobre requisicoes-POST

--- 14-requisicoes-post/index.js
+++ 14-requisicoes-post/index.js
@@ -28,8 +28,27 @@ document.addEventListener('DOMContentLoaded', () =>{
     fetchArticles()
 })
 
-document.querySelector('form')
+const form = document.querySelector('form')
 
-Form.addEventListener('submit', async (ev) => {
+form.addEventListener('submit', async (ev) => {
 ev.PreventDefault()
+
+const articleData = {
+    title: document.querySelector('#title'). value,
+    author: document.querySelector('#author').value,
+    content: document.querySelector('#content').value
+}
+
+const response = await fetch('http://localhost:3000/articles', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(articleData)
+})
+
+const savedArticle = await response.json()
+form.reset()
+renderArticle(savedArticle)
+console.log(savedArticle)
 })
